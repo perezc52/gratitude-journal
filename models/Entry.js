2cd8a4aa70561630d6d040c49c92358a2b0ff017
Carlos Perez <pcarlos132@yahoo.com>
2023-04-03 19:31:22 -0500

    Update Entry.js

--- models/Entry.js
+++ models/Entry.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const journaEntrylSchema = new mongoose.Schema({
+const journalEntrySchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
