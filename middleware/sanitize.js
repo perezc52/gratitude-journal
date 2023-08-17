e46953518414cc2e3af0e2403d79c214a431f121
Carlos Perez <pcarlos132@yahoo.com>
2023-08-16 20:40:43 -0500

    added html sanitization

---
+++ middleware/sanitize.js
@@ -0,0 +1,16 @@
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+module.exports = {
+    sanitizeHTML: (req, res, next) => {
+        for (const key in req.body) {
+            if (Object.hasOwnProperty.call(req.body, key)) {
+                req.body[key] = DOMPurify.sanitize(req.body[key]);
+            }
+        }
+        next();
+    },
+};
